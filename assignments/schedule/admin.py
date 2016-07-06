d46f8b677113574b600b6d6b5ac0065eca22ffd5
Andrew Kroll <krolla@gmail.com>
2016-07-06 16:28:29 -0400

    Base for parts/assignments

--- assignments/schedule/admin.py
+++ assignments/schedule/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Account
+from .models import PartCategory, Part, Assignment
+
+
+# User/Account
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+
+
+# New user admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (AccountInline, )
+    list_display = ('username', 'first_name', 'last_name', 'is_active', 'account_initials', 'account_khsid')
+
+    def account_initials(self, admin):
+        return admin.account.initials
+    account_initials.short_description = 'Initials'
+
+    def account_khsid(self, admin):
+        return admin.account.khsid
+    account_khsid.short_description = 'KHS ID'
+
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
+admin.site.register(PartCategory)
+admin.site.register(Part)
+admin.site.register(Assignment)
--- assignments/schedule/models.py
+++ assignments/schedule/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Account(models.Model):
+    initials = models.CharField(max_length=4)
+    khsid = models.IntegerField()
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.last_name + ', ' + self.user.first_name
+
+
+class PartCategory(models.Model):
+    name = models.CharField(max_length=64)
+    sort_order = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Part(models.Model):
+    name = models.CharField(max_length=64)
+    short_name = models.CharField(max_length=8)
+    sort_order = models.IntegerField()
+
+    category = models.ForeignKey(PartCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Assignment(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=256)
+
+    account = models.ForeignKey(Account)
+    part = models.ForeignKey(Part)
+
+    def __str__(self):
+        return self.date.strftime('%m/%d/%Y') + ': ' + str(self.account)
